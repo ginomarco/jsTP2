5cd4f299da1556f93a41a8d74f4cdfbb698f3122
ginomarco <ginotortorelli2019@gmail.com>
2023-07-20 20:20:30 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+const pizzas = [
+    {
+      id: 1,
+      nombre: "pizza de Muzzarella",
+      precio: 500,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    },
+  
+    {
+      id: 2,
+      nombre: "pizza de Cebolla",
+      precio: 1500,
+      ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    },
+  
+    {
+      id: 3,
+      nombre: "pizza Napolitana",
+      precio: 1350,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas"],
+    },
+  
+    {
+      id: 4,
+      nombre: "pizza 4 Quesos",
+      precio: 1380,
+      ingredientes: [
+        "Muzzarella",
+        "Tomate",
+        "Queso Azul",
+        "Parmesano",
+        "Roquefort",
+      ],
+    },
+  
+    {
+      id: 5,
+      nombre: "pizza Especial",
+      precio: 1000,
+      ingredientes: ["Muzzarella", "Tomate", "Aceitunas", "Anchoas", "Cebolla"],
+    },
+  
+    {
+      id: 6,
+      nombre: "pizza con Anana",
+      precio: 600,
+      ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    },
+  ];
+  
+  const Pizzas = [
+    {
+      id: 1,
+      nombre: "Margarita",
+      ingredientes: ["tomate", "mozzarella", "albahaca"],
+      precio: 500,
+    },
+    {
+      id: 2,
+      nombre: "Napolitana",
+      ingredientes: ["tomate", "mozzarella", "anchoas"],
+      precio: 550,
+    },
+    {
+      id: 3,
+      nombre: "Cuatro quesos",
+      ingredientes: ["mozzarella", "gorgonzola", "parmesano", "ricotta"],
+      precio: 600,
+    },
+    {
+      id: 4,
+      nombre: "Hawaiana",
+      ingredientes: ["tomate", "mozzarella", "jamón", "piña"],
+      precio: 650,
+    },
+  ];
+  
+  // a) Las pizzas que tengan un id impar.
+  console.log("Pizzas con ID impar:");
+  pizzas
+    .filter((pizza) => pizza.id % 2 !== 0)
+    .forEach((pizza) => console.log(`- ${pizza.nombre}`));
+  console.log("---------------");
+  
+  // b) Responder: ¿Hay alguna pizza que valga menos de $600?
+  const hayPizzaBarata = pizzas.some((pizza) => pizza.precio < 600);
+  console.log(
+    `¿Hay alguna pizza que valga menos de $600? ${hayPizzaBarata ? "Sí" : "No"}`
+  );
+  console.log("---------------");
+  
+  // c) El nombre de cada pizza con su respectivo precio.
+  console.log("Nombre y precio de cada pizza:");
+  pizzas.forEach((pizza) => console.log(`- ${pizza.nombre}: $${pizza.precio}`));
+  console.log("---------------");
+  
+  // d) Todos los ingredientes de cada pizza (En cada iteración imprimir los ingredientes de la pizza que se esta recorriendo).
+  console.log("Ingredientes de cada pizza:");
+  pizzas.forEach((pizza) => {
+    console.log(`- ${pizza.nombre}:`);
+    pizza.ingredientes.forEach((ingrediente) =>
+      console.log(`  - ${ingrediente}`)
+    );
+  });
+  console.log("---------------");
